10bda17f4c9a57e50e838994a1a77d0275acc44e
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2022-11-27 22:53:52 +0900

    Add files via upload

---
+++ GraphicsDrawLineMouseEx.java
@@ -0,0 +1,57 @@
+package graphics;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GraphicsDrawLineMouseEx extends JFrame{
+	private MyPanel panel = new MyPanel();
+	public GraphicsDrawLineMouseEx() {
+		setTitle("drawing Line by Mouse ¿¹Á¦");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setContentPane(panel);
+		setSize(300,300);
+		setVisible(true);
+	}
+	class MyPanel extends JPanel{
+		private Vector<Point>vStart = new Vector<Point>();
+		private Vector<Point>vEnd = new Vector<Point>();
+		public MyPanel() {
+			addMouseListener(new MouseAdapter() {
+				public void mousePressed(MouseEvent e) {
+					Point startP = e.getPoint();
+					vStart.add(startP);
+				}
+				public void mouseReleased(MouseEvent e) {
+					Point endP = e.getPoint();
+					vEnd.add(endP);
+					repaint();
+				}
+			});
+		}
+		@Override
+		public void paintComponent(Graphics g) {
+			
+			super.paintComponent(g);
+			g.setColor(Color.blue);
+			for(int i =0;i<vStart.size();i++) {
+				Point s = vStart.elementAt(i);
+				Point e = vEnd.elementAt(i);
+				g.drawLine((int)s.getX(), (int)s.getY(), (int)e.getX(), (int)e.getY());
+			}
+		}
+			
+	}
+
+	public static void main(String[] args) {
+		new GraphicsDrawLineMouseEx();
+	}
+
+}
